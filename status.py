e9edf0699267b82c9200af7ff5e759c703e7d1dd
bman <jtrain184@gmail.com>
2017-02-16 15:05:01 -0500

    Changed to return only status message

--- status.py
+++ status.py
@@ -3,11 +3,12 @@ import requests
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
+@app.route("/")
 def get_status():
-    url = 'https://www.opm.gov/json/operatingstatus.json'
-    response = jsonify(requests.get(url).json())
-    return response
+    url = "https://www.opm.gov/json/operatingstatus.json"
+    response = requests.get(url).json()
+    long_status_message = response["LongStatusMessage"]
+    return "Status: \n" + long_status_message
 
 if __name__ == "__main__":
     app.run()
